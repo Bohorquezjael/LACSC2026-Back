70e5ab8d26de4a315a982c2f07259413e7168a3a
Jael Bohorquez <Jaelbkz@icloud.com>
2025-06-21 13:58:19 -0600

    first commit with folders and files

---
+++ src/main/java/com/innovawebJT/lacsc/LacscApplication.java
@@ -0,0 +1,13 @@
+package com.innovawebJT.lacsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovawebJT/lacsc/LacscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovawebJT.lacsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
