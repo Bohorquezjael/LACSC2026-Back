7ebfdd30e16f461ea977b41aa8c4d0b3740264e2
Jael Alonso Bohorquez <jaelbkz@icloud.com>
2025-09-19 22:57:15 -0600

    add: repositrory, service and controler layer for summary

--- src/main/java/com/innovawebJT/lacsc/repository/SummaryRepository.java
+++ src/main/java/com/innovawebJT/lacsc/repository/SummaryRepository.java
@@ -2,7 +2,7 @@ package com.innovawebJT.lacsc.repository;
 
 import com.innovawebJT.lacsc.model.Summary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface SummaryRepository extends JpaRepository<Summary, Long>, PagingAndSortingRepository<Summary, Long> {
+public interface SummaryRepository extends JpaRepository<Summary, Long> {
+
 }
--- src/main/java/com/innovawebJT/lacsc/service/ISummaryService.java
+++ src/main/java/com/innovawebJT/lacsc/service/ISummaryService.java
@@ -1,4 +1,17 @@
 package com.innovawebJT.lacsc.service;
 
+import java.util.Optional;
+
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Page;
+
+import com.innovawebJT.lacsc.model.Summary;
+
 public interface ISummaryService {
+
+    Page<Summary> getAllSummaries(Pageable pageable);
+
+    Optional<Summary> getById(Long id);
+
+    void deleteSummary(Long id);
 }
