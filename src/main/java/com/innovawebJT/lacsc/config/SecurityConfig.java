1321360995520e810dd61e65418da4d44fa87fc0
Bohorquez <jaelbkz@icloud.com>
2025-09-22 16:41:19 -0600

    add: first attemp of twice login for other endpoint like h2, swagger, etc

--- src/main/java/com/innovawebJT/lacsc/config/SecurityConfig.java
+++ src/main/java/com/innovawebJT/lacsc/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.innovawebJT.lacsc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,7 +25,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
+                .authorizeHttpRequests(http -> http
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**")
+                        .hasRole("admin-client")
+                        .anyRequest().authenticated())
+                .oauth2Login(withDefaults())
                 .oauth2ResourceServer(oauth -> {
                     oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtRolesConverter));
                 })
