49fac1936861bd9365aea325080aa89907e104b8
Bohorquez <jaelbkz@icloud.com>
2025-09-23 11:49:49 -0600

    add: correct functionality, keycloak manages login to dev endpoints and all others by stateless fix endpoints by adding "/api/"

--- src/main/java/com/innovawebJT/lacsc/config/SecurityConfig.java
+++ src/main/java/com/innovawebJT/lacsc/config/SecurityConfig.java
@@ -22,38 +22,39 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final JwtRolesConverter jwtRolesConverter;
+        private final JwtRolesConverter jwtRolesConverter;
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .securityMatcher(new AntPathRequestMatcher("/api/**"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtRolesConverter)))
-                .sessionManagement(sessionMg -> sessionMg.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
-    }
+        @Bean
+        @Order(1)
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                return httpSecurity
+                                .securityMatcher(new AntPathRequestMatcher("/api/**"))
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
+                                .oauth2ResourceServer(oauth -> oauth
+                                                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtRolesConverter)))
+                                .sessionManagement(sessionMg -> sessionMg
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .build();
+        }
 
-    @Bean
-    @Order(2)
-    public SecurityFilterChain devSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                        )
-                )
-                .authorizeHttpRequests(http -> http
-                        .requestMatchers("/h2-console/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").authenticated()
-                        .anyRequest().permitAll())
-                .oauth2Login(withDefaults())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID"))
-                .build();
-    }
+        @Bean
+        @Order(2)
+        public SecurityFilterChain devSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                return httpSecurity
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .headers(headers -> headers
+                                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                                .authorizeHttpRequests(http -> http
+                                                .requestMatchers("/h2-console/**", "/swagger-ui/**", "/swagger-ui.html",
+                                                                "/v3/api-docs/**")
+                                                .authenticated())
+                                .oauth2Login(withDefaults())
+                                .logout(logout -> logout
+                                                .logoutSuccessUrl("/")
+                                                .invalidateHttpSession(true)
+                                                .clearAuthentication(true)
+                                                .deleteCookies("JSESSIONID"))
+                                .build();
+        }
 }
